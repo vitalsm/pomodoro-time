1e531befd4e53bcecde89219296b4c3ece79fe8d
vitalsm <37532908+vitalsm@users.noreply.github.com>
2025-06-21 22:54:10 +0300

    Merge pull request #2
    * refactoring: (Infra) use async connection in DB, Cache, Client

--- clients/google.py
+++ clients/google.py
@@ -13,12 +13,11 @@ class GoogleClient:
     async_client: httpx.AsyncClient
 
     async def get_user_info(self, code: str) -> GoogleUserData:
-        access_token = self._get_user_access_token(code=code)
-        async with self.async_client as client:
-            user_info = await client.get(
-                'https://www.googleapis.com/oauth2/v1/userinfo',
-                headers={'Authorization': f'Bearer {access_token}'}
-            )
+        access_token = await self._get_user_access_token(code=code)
+        user_info = await self.async_client.get(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
         return GoogleUserData(**user_info.json(), access_token=access_token)
 
     async def _get_user_access_token(self, code: str) -> str:
@@ -29,13 +28,12 @@ class GoogleClient:
             'redirect_uri': self.settings.GOOGLE_REDIRECT_URI,
             'grant_type': 'authorization_code'
         }
-        async with self.async_client as client:
-            response = await client.post(self.settings.GOOGLE_TOKEN_URI, data=data)
+        response = await self.async_client.post(self.settings.GOOGLE_TOKEN_URI, data=data)
 
-            if response.status_code == 200:
-                return response.json()['access_token']
-            else:
-                raise HTTPException(
-                    status_code=response.status_code,
-                    detail=response.json()['error_description']
-                )
+        if response.status_code == 200:
+            return response.json()['access_token']
+        else:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=response.json()['error_description']
+            )
--- service/auth.py
+++ service/auth.py
@@ -25,7 +25,7 @@ class AuthService:
     password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
     def get_hashed_password(self, password: str) -> str:
-        return self.password_context.using(rounds=12).hash(password)
+        return self.password_context.hash(password)
 
     def verify_password(self, password: str, hashed_pass: str) -> bool:
         return self.password_context.verify(password, hashed_pass)
